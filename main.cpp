ff7eec4a8eb6506598010cff988e256f6b2a9da9
Samuel <baceceo@gmail.com>
2024-10-31 18:54:49 +0000

    day1, data, memory, variables, cout, cin

--- main.cpp
+++ main.cpp
@@ -1,21 +1,6 @@
-#include <iostream>
-#include <string>
-
+#include "day1.h"
 
 int main() {
-    std::string my_string {"hello world\n"};
-    std::cout << my_string;
-    int x;
-    int y;
-    int z;
-    std::cout << "Enter 3 numbers: ";
-    std::cin >> x;
-    std::cin.clear();
-    std::cin >> y;
-    std::cin.clear();
-    std::cin >> z;
-    std::cout << "x = " << x << "\n";
-    std::cout << "y = " << y << "\n";
-    std::cout << "z = " << z << "\n";
+    day1();
     return 0;
 }
